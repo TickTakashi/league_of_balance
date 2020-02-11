f2dd8bec4cfdf036bd6d95285dfdaf56279cae97
Charles Gbadamosi <acw524@qmul.ac.uk>
2020-02-10 19:31:46 -0800

    Adds code to progressively download all data. Adds functionality to sort pages downloaded into sensible folders.

--- main.py
+++ main.py
@@ -2,6 +2,21 @@ import requests
 from datetime import datetime
 import time
 import sys
+import os
+
+from os import path
+
+HEADERS = {
+	'X-Riot-Token': 'RGAPI-e463d50f-3808-4ca5-8b7c-5a0805169c5f',
+}
+
+
+leagues_5x5_uri = "https://{region}.api.riotgames.com/lol/league/v4/entries/RANKED_SOLO_5x5/{tier}/{division}?page={{page_index}}"	
+
+seconds_between_requests = 1.2
+tiers = ["DIAMOND", "PLATINUM", "GOLD", "SILVER", "BRONZE", "IRON"]
+divisions = ["I", "II", "III", "IV"]
+reg = "na1"
 
 def find_size(accessor):
 	index = 1
@@ -52,9 +67,6 @@ def page_poller(uri, **kwargs):
 	formatted = uri.format(**kwargs)
 	print("Constructing Poll Function for: %s" % formatted)
 
-	HEADERS = {
-		'X-Riot-Token': 'RGAPI-e463d50f-3808-4ca5-8b7c-5a0805169c5f',
-	}
 
 	def poller(index):
 		t0 = datetime.now()
@@ -62,7 +74,7 @@ def page_poller(uri, **kwargs):
 
 		r = requests.get(formatted.format(page_index=index), headers=HEADERS)
 		
-		while (datetime.now() - t0).total_seconds() < 1.2:
+		while (datetime.now() - t0).total_seconds() < seconds_between_requests:
 			time.sleep(0.05)
 
 		if r.ok and r.text != "[]":
@@ -74,42 +86,119 @@ def page_poller(uri, **kwargs):
 		
 	return poller
 
-# HEADERS = {
-# 	'X-Riot-Token': 'RGAPI-e463d50f-3808-4ca5-8b7c-5a0805169c5f',
-# }
 
-# size = find_size(page_poller(leagues_5x5_uri, region="euw1", tier="DIAMOND", division="I"))
+def discover_sizes(file):
+	division_size_dict = {}
+	for t in tiers:
+		for d in divisions:
+			division_size_dict[(t, d)] = find_size(page_poller(leagues_5x5_uri, region="na1", tier=t, division=d))
+			print("%s - %s has page size %s" % (t, d, division_size_dict[(t, d)]))
+			file.write("%s,%s,%s\n" % (t, d, division_size_dict[(t, d)]))
+	return division_size_dict
 
-# print("Discovered Size: %s" % size)
 
-# time.sleep(1.2)
+def riot_get(uri):
+	t0 = datetime.now()
+	#print("[%s] - Attempting GET %s" % (t0, uri))
+	
+	r = requests.get(uri, headers=HEADERS)
+	
+	while (datetime.now() - t0).total_seconds() < seconds_between_requests:
+		time.sleep(0.05)
 
-# r = requests.get(leagues_5x5_uri.format(region="euw1", tier="DIAMOND", division="I").format(page_index=size), headers=HEADERS)
+	if r.ok and r.text != "[]":
+		#print("[%s] - GET Success - code %s" % (datetime.now(), r.status_code))
+		return r.text
+	else:
+		print("[%s] - GET FAIL - HTTP Code %s, Content %s" % (datetime.now(), r.status_code, r.text))
+		return False
 
-# if r.ok and r.text != "[]":
-# 	print("SUCCESS: Polled %s Successfully with code: %s, \nContent: %s" % (size, r.status_code, r.text))
-# else:
-# 	print("FAILED: Code %s and Content %s" % (r, r.text))
 
-filename = "standard_play_league_page_sizes.txt"
+def get_division_page(region, tier, division, page_index):
+	uri = leagues_5x5_uri.format(region=region, tier=tier, division=division).format(page_index=page_index)
+	#print("[%s] - Reading page %s of (%s, %s, %s)" % (datetime.now(), page_index,region, tier, division))
+	return riot_get(uri)
+
+
+def stamped_print(instr):
+	print("[%s] - %s" % (datetime.now(), instr))
+	
+
+folder_name = "data"
+size_file = "standard_play_league_page_sizes.txt"
 
 if len(sys.argv) > 1:
-	filename = sys.argv[1]
-	print("Writing to file: %s" % filename)
+	folder_name = sys.argv[1]
+	stamped_print("Writing to folder: %s" % folder_name)
 else:
-	print("No filename given, writing to file: %s" % filename)
+	stamped_print("No folder_name given, writing to folder: %s" % folder_name)
+
+if not path.exists(folder_name):
+	os.mkdir(folder_name)
+else:
+	stamped_print("Warning. Path %s exists. Overwriting data inside." % folder_name)
+
+
+stamped_print("Reading Page Count File")
+
+page_sizes = {}
+
+try:
+	f = open(size_file, "r")
+	p_size_str = f.read()
+	ps = p_size_str.strip().split("\n")
+	
+	for p_size in ps:
+		line = p_size.strip().split(",")
+		page_sizes[(line[0], line[1])] = int(line[2])
+except IOError:
+	stamped_print("Page size file doesn't exist. Generating now...")
+	page_sizes = discover_sizes(open(size_file, "w+"))
+	stamped_print("Page size file Generated.")
+	stamped_print("Page size dictionary\n%s" % page_sizes)
+finally:
+	f.close()
+
+
+stamped_print("Page Counts Loaded")
+stamped_print(page_sizes)
+
+
+for t in tiers:
+	tier_folder = os.path.join(folder_name, t)
+	
+	if not os.path.exists(tier_folder):
+		os.mkdir(tier_folder)
+	
+	for d in divisions:
+		division_folder = os.path.join(tier_folder, d)
+	
+		if not os.path.exists(division_folder):
+			os.mkdir(division_folder)
+
+		sz = page_sizes[(t,d)]
+		stamped_print("Downloading Rankings for %s %s (%s Pages Expected)" % (t, d, sz))
+		
+		for i in range(1, sz + 1):
+			pagestr = get_division_page(reg, t, d, i)
+			if pagestr:
+				stamped_print("[%s %s (Page %s/%s)] SUCCESS" % (t, d, i, sz))
+				
+				pagefile_out = os.path.join(division_folder, str(i).zfill(6))
+
+				f = open(pagefile_out, "w")
+				f.write(pagestr)
+				f.close()
+			else:
+				stamped_print("[%s %s (Page %s/%s)] FAIL - QUITTING" % (t, d, i, sz))
+				exit()
+'''
+
 
 
 f = open(filename,"w+")
 
-leagues_5x5_uri = "https://{region}.api.riotgames.com/lol/league/v4/entries/RANKED_SOLO_5x5/{tier}/{division}?page={{page_index}}"
-
-division_size_dict = {}
-
-for t in ["DIAMOND", "PLATINUM", "GOLD", "SILVER", "BRONZE", "IRON"]:
-	for d in ["I", "II", "III", "IV"]:
-		division_size_dict[(t, d)] = find_size(page_poller(leagues_5x5_uri, region="na1", tier=t, division=d))
-		print("%s - %s has page size %s" % (t, d, division_size_dict[(t, d)]))
-		f.write("[\"%s\", \"%s\", %s]\n" % (t, d, division_size_dict[(t, d)]))
+f.write(get_division_page("na1", "GOLD", "I", 1))
 
 f.close()
+'''
